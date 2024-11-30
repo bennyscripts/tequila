337c1e4c5c577d8cfd03107906371ce619275b93
Ben Tettmar <bentettmar@yahoo.com>
2024-11-30 17:03:51 +0000

    Initial Commit

---
+++ Tequila/TequilaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TequilaApp.swift
+//  Tequila
+//
+//  Created by Ben Tettmar on 30/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TequilaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TequilaTests/TequilaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TequilaTests.swift
+//  TequilaTests
+//
+//  Created by Ben Tettmar on 30/11/2024.
+//
+
+import Testing
+@testable import Tequila
+
+struct TequilaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
