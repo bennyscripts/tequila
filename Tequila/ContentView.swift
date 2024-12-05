f4ed9aa2b68f9747917303a20c8b4d9725d8a371
Ben Tettmar <bentettmar@yahoo.com>
2024-12-05 00:44:55 +0000

    Move ViewModel to main app file

--- Tequila/ContentView.swift
+++ Tequila/ContentView.swift
@@ -7,34 +7,8 @@
 
 import SwiftUI
 
-class ViewModel: ObservableObject {
-    @Published var gamesList = Games()
-    @Published var favourites = Favourites()
-    @Published var webService = WebService()
-    @Published var showRefreshButton = false
-    @Published var refreshButtonCooldown = false
-    
-    func updateView() {
-        self.objectWillChange.send()
-    }
-    
-    func refresh() {
-        if !refreshButtonCooldown {
-            self.gamesList.refresh()
-            self.favourites.save()
-            self.refreshButtonCooldown = true
-        } else {
-            print("Cooldown in effect! Please dont spam my API 🙏")
-        }
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
-            self.refreshButtonCooldown = false
-        }
-    }
-}
-
 struct ContentView: View {
-    @ObservedObject var model = ViewModel()
+    @ObservedObject var model: ViewModel
     
     var body: some View {
         NavigationView {
--- Tequila/TequilaApp.swift
+++ Tequila/TequilaApp.swift
@@ -7,11 +7,44 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var gamesList = Games()
+    @Published var favourites = Favourites()
+    @Published var webService = WebService()
+    @Published var cache = GamesCache()
+    @Published var showRefreshButton = false
+    @Published var refreshButtonCooldown = false
+    
+    init() {
+        print("ViewModel init")
+    }
+    
+    func updateView() {
+        self.objectWillChange.send()
+    }
+    
+    func refresh() {
+        if !refreshButtonCooldown {
+            self.gamesList.refresh()
+            self.favourites.save()
+            self.refreshButtonCooldown = true
+        } else {
+            print("Cooldown in effect! Please dont spam my API 🙏")
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.refreshButtonCooldown = false
+        }
+    }
+}
+
 @main
 struct TequilaApp: App {
+    @ObservedObject var model = ViewModel()
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(model: model)
         }
         .windowResizability(.contentSize)
 //        .defaultSize(width: 880, height: 400)
