b0bc8a1be9ca82991f925295248402907815df7f
Ben Tettmar <bentettmar@yahoo.com>
2024-12-10 14:35:59 +0000

    Added info view, needs work

---
+++ Tequila/Views/InfoView.swift
@@ -0,0 +1,16 @@
+//
+//  InfoView.swift
+//  Tequila
+//
+//  Created by Ben Tettmar on 10/12/2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Hello World")
+        }
+    }
+}
